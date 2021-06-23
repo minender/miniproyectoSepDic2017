75fb40bc47ced81278960d6e9ba3789117e0e24f
Manuel Faria <mfaria724@gmail.com>
2021-06-23 18:01:47 +0100

    Documenting middleware and setting env variables for logging server.

--- src/main/java/com/calclogic/controller/RequestInterceptor.java
+++ src/main/java/com/calclogic/controller/RequestInterceptor.java
@@ -10,20 +10,36 @@ import org.apache.log4j.Logger;
 
 public class RequestInterceptor extends HandlerInterceptorAdapter {
 
+	// starts logger for interceptor class
     static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(RequestInterceptor.class);
 
+	/**
+	 * Handle every request after it has been completly processed.
+	 * @param request incoming request object
+	 * @param response outgoing response
+	 * @param handler
+	 * @param ex
+	 */
 	@Override
 	public void afterCompletion(
-	HttpServletRequest request, 
-	HttpServletResponse response,
-	Object handler, Exception ex) {
-		String log = "SOURCE " + request.getRemoteAddr() + ":" + request.getRemotePort();
+		HttpServletRequest request, 
+		HttpServletResponse response,
+		Object handler, Exception ex
+	) 
+	{
+
+		String log = "";
+
+		// source IP request
+		log += "SOURCE " + request.getRemoteAddr() + ":" + request.getRemotePort();
+		// method requested
 		log += " - METHOD " + request.getMethod();
+		// URL requested
 		log += " - URI " + request.getRequestURI();
+		// response status
 		log += " - STATUS " + response.getStatus();
+
 		logger.info(log);
-
-
 	}
 
 }
