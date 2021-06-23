3b31acf3c8dc7aac9a8fdc53fefa60ec3293aa92
Manuel Faria <mfaria724@gmail.com>
2021-06-23 10:47:17 +0100

    Create configuration for remote logging with log4j. Commit to test deployment on Heroku with remote logging.

--- src/main/java/com/calclogic/controller/RequestInterceptor.java
+++ src/main/java/com/calclogic/controller/RequestInterceptor.java
@@ -6,15 +6,24 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+// import org.apache.log4j.PrettyConfigurator;
+import org.apache.log4j.Logger;
+// import org.apache.log4j.PropertyConfigurator;
+
 public class RequestInterceptor extends HandlerInterceptorAdapter {
+
+    static final org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(RequestInterceptor.class);
 
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
+		System.out.println("Testing...");
+		logger.error("Testing logger...");
+		// PropertyConfigurator.configure("../../../../resources/log4j2.properties");
+
 		long startTime = System.currentTimeMillis();
-        System.out.println("preHandle");
-        System.out.println("Request URL::" + request.getRequestURL().toString()
-        + ":: Start Time=" + System.currentTimeMillis());
+        // logger.error("Request URL::" + request.getRequestURL().toString()
+        // + ":: Start Time=" + System.currentTimeMillis());
 		return true;
 	}
 
@@ -22,21 +31,19 @@ public class RequestInterceptor extends HandlerInterceptorAdapter {
 	public void postHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-        System.out.println("postHandle");
-		System.out.println("Request URL::" + request.getRequestURL().toString()
-				+ " Sent to Handler :: Current Time=" + System.currentTimeMillis());
+        // logger.error("Request URL::" + request.getRequestURL().toString()
+        // + " Sent to Handler :: Current Time=" + System.currentTimeMillis());
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request,
 			HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
-        System.out.println("afterCompletion");
 		long startTime = (Long) request.getAttribute("startTime");
-        System.out.println("Request URL::" + request.getRequestURL().toString()
-        + ":: End Time=" + System.currentTimeMillis());
-        System.out.println("Request URL::" + request.getRequestURL().toString()
-        + ":: Time Taken=" + (System.currentTimeMillis() - startTime));
+        // logger.error("Request URL::" + request.getRequestURL().toString()
+        // + ":: End Time=" + System.currentTimeMillis());
+        // logger.error("Request URL::" + request.getRequestURL().toString()
+        // + ":: Time Taken=" + (System.currentTimeMillis() - startTime));
 	}
 
 }
