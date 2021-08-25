41b964c00d6c442e46244a359f65846756db65f7
RonaldBecerra <12-10706@usb.ve>
2021-08-25 17:36:15 -0400

    Restauré los archivos de Publicacion en DAO porque si no da un error

---
+++ src/main/java/com/calclogic/dao/PublicacionDAO.java
@@ -0,0 +1,46 @@
+package com.calclogic.dao;
+
+import com.calclogic.entity.Publicacion;
+import com.calclogic.entity.PublicacionId;
+import java.util.List;
+
+
+/**
+ * This interface is the api of the database queries that 
+ * have to do with the table "Publicacion". 
+ *
+ * @author federico
+ */
+public interface PublicacionDAO {
+    
+    /** 
+     * Adds a new Publicacion object to the table.
+     * @param publicacion The new Predicado object to be added.
+     * @return Nothing.
+     */
+    public void addPublicacion(Publicacion publicacion);
+
+    /**
+     * Deletes one of the Publicacion objects of the table.
+     * @param id Is the principal key of the Publicacion object to delete.
+     * @return Nothing.
+     */ 
+    public void deletePublicacion(PublicacionId id);
+
+    /**
+     * Method to get a Publicacion object by its principal key.
+     * @param id Is the principal key of the Publicacion object.
+     */  
+    public Publicacion getPublicacion(PublicacionId id);
+
+    /**
+     * Method to get a list of all the entries of the table.
+     */
+    public List<Publicacion> getAllPublicaciones();
+
+    /**
+     * Method to get a list of all the entries of the table that correspond to a specific user.
+     * @param username Is the string with which the user logs in, and that we use to filter the search.
+     */
+    public List<Publicacion> getAllPublicaciones(String username);
+}
---
+++ src/main/java/com/calclogic/dao/PublicacionDaoImpl.java
@@ -0,0 +1,80 @@
+package com.calclogic.dao;
+
+import com.calclogic.entity.Publicacion;
+import com.calclogic.entity.PublicacionId;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * This class has the implementation of the database queries that 
+ * have to do with the table "Publicacion".
+ *
+ * @author federico
+ */
+@Repository
+public class PublicacionDaoImpl implements PublicacionDAO{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    /** 
+     * Adds a new Publicacion object to the table.
+     * This query is made using standard Hibernate library functions.
+     * @param publicacion The new Predicado object to be added.
+     * @return Nothing.
+     */
+    @Override
+    public void addPublicacion(Publicacion publicacion) {
+        this.sessionFactory.getCurrentSession().save(publicacion);
+    }
+
+    /**
+     * Deletes one of the Publicacion objects of the table.
+     * This query is made using standard Hibernate library functions.
+     * @param id Is the principal key of the Publicacion object to delete.
+     * @return Nothing.
+     */ 
+    @Override
+    public void deletePublicacion(PublicacionId id) {
+        Publicacion publicacion = (Publicacion) sessionFactory.getCurrentSession().load(
+				Publicacion.class, id);
+        if (null != id) {
+        	this.sessionFactory.getCurrentSession().delete(publicacion);
+        }
+    }
+
+    /**
+     * Method to get a Publicacion object by its principal key.
+     * This query is made using standard Hibernate library functions.
+     * @param id Is the principal key of the Publicacion object.
+     */  
+    @SuppressWarnings("unchecked")
+    @Override
+    public Publicacion getPublicacion(PublicacionId id) {
+        return (Publicacion)this.sessionFactory.getCurrentSession().get(Publicacion.class,id);
+    }
+
+    /**
+     * Method to get a list of all the entries of the table.
+     * This query is made using classic SQL.
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Publicacion> getAllPublicaciones() {
+        return this.sessionFactory.getCurrentSession().createQuery("FROM Publicaciones").list();
+    }
+
+    /**
+     * Method to get a list of all the entries of the table that correspond to a specific user.
+     * This query is made using classic SQL.
+     * @param username Is the string with which the user logs in, and that we use to filter the search.
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Publicacion> getAllPublicaciones(String username) {
+        return this.sessionFactory.getCurrentSession().createQuery("FROM Publicaciones WHERE usuario.login = :username").setParameter("username",username).list();
+    }
+    
+}
