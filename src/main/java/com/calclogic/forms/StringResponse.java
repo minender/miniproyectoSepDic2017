4ca7f535d74c9fa9a23fe40d68eb42dce2108291
Ronald <12-10706@usb.ve>
2022-08-02 19:28:55 -0400

    Probando metateoremas

---
+++ src/main/java/com/calclogic/forms/StringResponse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.calclogic.forms;
+
+/**
+ * This is only used to send a simple String to an AJAX
+ * 
+ * @author ronald
+ */
+
+
+public class StringResponse {
+    
+    private String string;
+    
+    public StringResponse(String string) {
+        this.string = string;
+    }
+}
