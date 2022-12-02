e2aacc387fd68dd0df3b9246398ac670b8d4df47
Ronald <12-10706@usb.ve>
2022-12-01 22:40:53 -0400

    EN proceso se arreglar partir de un lado

--- src/main/java/com/calclogic/proof/StartingOneSideMethodImpl.java
+++ src/main/java/com/calclogic/proof/StartingOneSideMethodImpl.java
@@ -260,7 +260,14 @@ public class StartingOneSideMethodImpl extends GenericProofMethodImpl implements
     {
         // If Formula that the user is trying to prove in this proof/sub-proof is of the form H => A == B, then H /\ A ==  H /\ B must be given instead)
         if(initialExpr.equals(((App)((App)formulaBeingProved).p).q) && finalExpr.equals(((App)formulaBeingProved).q)){
+            Term operatorTerm = ((App)((App)formulaBeingProved).p).p;
+            // The Starting From One Side method only admits reflexive operators
+            int res = resuelveManager.isReflexiveOperatorForUser(username, operatorTerm.toString());
+            
+            TypedApp app = new TypedApp(new TypedS(proof.type()), proof);
+
             return new TypedApp(new TypedS(proof.type()), proof);
+
         }
         return proof;
     }
